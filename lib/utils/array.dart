2448a68a255000e29d042e32c6c954defa38e2fb
Temur Xoldarov <temuruz2003@gmail.com>
2022-10-21 13:45:48 +0500

    ui

---
+++ lib/utils/array.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+List<TextSpan> highlightOccurrences(String source, String query) {
+  if (query.isEmpty || !source.toLowerCase().contains(query.toLowerCase())) {
+    return [TextSpan(text: source)];
+  }
+  final matches = query.toLowerCase().allMatches(source.toLowerCase());
+
+  int lastMatchEnd = 0;
+
+  final List<TextSpan> children = [];
+  for (var i = 0; i < matches.length; i++) {
+    final match = matches.elementAt(i);
+
+    if (match.start != lastMatchEnd) {
+      children.add(TextSpan(
+        text: source.substring(lastMatchEnd, match.start),
+      ));
+    }
+
+    children.add(TextSpan(
+      text: source.substring(match.start, match.end),
+      style: const TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
+    ));
+
+    if (i == matches.length - 1 && match.end != source.length) {
+      children.add(TextSpan(
+        text: source.substring(match.end, source.length),
+      ));
+    }
+
+    lastMatchEnd = match.end;
+  }
+  return children;
+}
